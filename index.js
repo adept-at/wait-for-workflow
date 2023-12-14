01d35ec2ba81d6068dbb553b8c94b567b9e44c0f
pmerwin <pmerwin@cbtnuggets.com>
2023-12-14 13:29:25 -0800

    use core for output

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ async function run() {
   const MAX_ATTEMPTS = 8;
   let attempt = 0;
   const current_time = new Date();
-  console.log('Current Time: ', current_time.toISOString());
+  core.info('Current Time: ' + current_time.toISOString());
   const thirtySecsLater = new Date(current_time.getTime() + 30000);
 
   async function checkWorkflowStatus() {
@@ -23,6 +23,7 @@ async function run() {
         'User-Agent': 'Node.js Script',
       },
     };
+    core.info('Request options path: ' + JSON.stringify(options.path));
 
     return new Promise((resolve, reject) => {
       https
@@ -34,11 +35,13 @@ async function run() {
           res.on('end', () => {
             const response = JSON.parse(data);
             if (!response.workflow_runs) {
-              console.error('Unexpected response structure:', response);
+              core.error(
+                'Unexpected response structure: ' + JSON.stringify(response)
+              );
               reject('Invalid API response');
               return;
             }
-            console.log('Response:', response.workflow_runs[0].status);
+            core.info('Response: ' + response.workflow_runs[0].status);
             const workflowRuns = response.workflow_runs.filter(
               (run) =>
                 new Date(run.created_at) > new Date(current_time) &&
@@ -49,27 +52,28 @@ async function run() {
             if (workflowRuns.length > 0) {
               const status = workflowRuns[0].status;
               const conclusion = workflowRuns[0].conclusion;
-              console.log('Status of the matching workflow run: ', status);
+              core.info('Status of the matching workflow run: ' + status);
 
               if (status === 'completed') {
-                console.log(
-                  'Completed matching workflow run: ',
-                  status,
-                  conclusion
+                core.info(
+                  'Completed matching workflow run: ' +
+                    status +
+                    ', ' +
+                    conclusion
                 );
                 resolve({ status, conclusion });
               } else {
-                console.log('Workflow run is not completed yet');
+                core.info('Workflow run is not completed yet');
                 resolve({ status, conclusion: null });
               }
             } else {
-              console.log('No workflow runs found');
+              core.info('No workflow runs found');
               reject('No workflow runs found');
             }
           });
         })
         .on('error', (e) => {
-          console.error(e);
+          core.error('HTTP request failed: ' + e.message);
           reject(e);
         });
     });
@@ -81,34 +85,34 @@ async function run() {
       try {
         res = await checkWorkflowStatus();
       } catch (error) {
-        console.error('Error checking workflow status:', error);
+        core.error('Error checking workflow status: ' + error.message);
       }
 
       if (res && res.status === 'completed' && res.conclusion === 'success') {
-        console.log('Workflow completed and test passed!');
+        core.info('Workflow completed and test passed!');
         break;
       } else if (
         res &&
         res.status === 'completed' &&
         res.conclusion === 'failure'
       ) {
-        console.log('Workflow status is failed...');
+        core.error('Workflow status is failed...');
         process.exit(1);
       } else if (res?.status !== '') {
-        console.log(
+        core.info(
           'Workflow status is ' + res?.status + '. Waiting for completion...'
         );
       } else {
-        console.log('Workflow status is unknown. Waiting for completion...');
+        core.info('Workflow status is unknown. Waiting for completion...');
       }
 
       attempt++;
-      console.log('Attempt: ' + attempt);
+      core.info('Attempt: ' + attempt);
       await new Promise((resolve) => setTimeout(resolve, 60000)); // 60 seconds
     }
 
     if (attempt === MAX_ATTEMPTS) {
-      console.log('Max attempts reached without completion. Exiting.');
+      core.error('Max attempts reached without completion. Exiting.');
       process.exit(1);
     }
   }
@@ -117,5 +121,5 @@ async function run() {
 }
 
 run().catch((error) => {
-  core.setFailed(error.message);
+  core.setFailed('Action failed with error: ' + error.message);
 });
